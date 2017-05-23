bf38f502dd668d00f1ab5a81437febcbd5a0b194
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-05-23 13:32:37 -0700

    Bug Fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523010815) do
+ActiveRecord::Schema.define(version: 20170523171841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20170523010815) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "creator_id", null: false
+    t.string   "art_url"
+    t.boolean  "public",     null: false
     t.index ["creator_id"], name: "index_playlists_on_creator_id", using: :btree
     t.index ["title"], name: "index_playlists_on_title", using: :btree
   end
